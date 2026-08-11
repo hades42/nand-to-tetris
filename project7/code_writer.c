32b07688c9f53189af66ee31d140e3e6073b7cb9
hades42 <nguyenvannguyen.oc@gmail.com>
2026-08-11 22:00:17 +1000

    t

--- project7/code_writer.c
+++ project7/code_writer.c
@@ -1,6 +1,9 @@
 #include "code_writer.h"
 
-int code_writer_open(CodeWriter *code_writer, char *filename) {
+int code_writer_open(CodeWriter *code_writer, const char *filename) {
     // change file extension 
     char *new_file[1024];
+    int read = 0;
+    int write = 0;
+    while(filename[read] != '\0' && filename[read] != )
 }
