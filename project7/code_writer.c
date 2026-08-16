44add7d992d1beab43668dbeafea4fa3d74cbf34
hades42 <nguyenvannguyen.oc@gmail.com>
2026-08-16 15:11:17 +1000

    temp and pointer segment

--- project7/code_writer.c
+++ project7/code_writer.c
@@ -26,51 +26,114 @@ void code_writer_push_pop(
     int index
 ) {
     FILE *output_file = code_writer->file; 
+    char *base = NULL;
+
+    if (strcmp(segment, "local") == 0) {
+        base = "LCL";
+    } else if (strcmp(segment, "argument") == 0) {
+        base = "ARG";
+    } else if (strcmp(segment, "this") == 0) {
+        base = "THIS";
+    } else if (strcmp(segment, "that") == 0) {
+        base = "THAT";
+    }
 
     if (type == C_PUSH) {
-        char *base = NULL;
-        if (strcmp(segment, "local") == 0) {
-            base = "LCL";
-        } else if (strcmp(segment, "argument") == 0) {
-            base = "ARG";
-        } else if (strcmp(segment, "this") == 0) {
-            base = "THIS";
-        } else if (strcmp(segment, "that") == 0) {
-            base = "THAT";
-        }
-
-        if (base != NULL) {
+        
+        // Get value out of segment and store in D
+        if (strcmp(segment, "constant") == 0) {
+            fprintf(output_file, "@%d\n", index);        
+            fprintf(output_file, "D=A\n");
+        } else if (base != NULL) {
             fprintf(output_file, "@%d\n", index);
             fprintf(output_file, "D=A\n");
             fprintf(output_file, "@%s\n", base);
             fprintf(output_file, "A=D+M\n");
             fprintf(output_file, "D=M\n");
+        } else if (strcmp(segment, "temp") == 0) {
+            if (index < 0 || index > 7) {
+                fprintf(stderr, "Error: temp index can only go from range 0 to 7");
+                return;
+            } else {
+                fprintf(output_file, "@%d\n", index + 5);
+                fprintf(output_file, "D=M\n");
+            }
+        } else if (strcmp(segment, "pointer") == 0) {
+            if (index == 0 || index == 1) {
+                if (index == 0) {
+                    fprintf(output_file, "@THIS\n");
+                } else {
+                    fprintf(output_file, "@THAT\n");
+                }
+                fprintf(output_file, "D=M\n");
+            } else {
+                fprintf(stderr, "Error: pointer index can only be 0 or 1");
+                return;
+            }
+        } else {
+            fprintf(stderr, "Error: Unknown segment in push");
+            return;
         }
 
-        if (strcmp(segment, "constant") == 0) {
-            fprintf(output_file, "@%d\n", index);        
-            fprintf(output_file, "D=A\n");
-            fprintf(output_file, "@SP\n");
-            fprintf(output_file, "A=M\n");
-            fprintf(output_file, "M=D\n");
-            fprintf(output_file, "@SP\n");
-            fprintf(output_file, "M=M+1\n");
-        } else if (
-            strcmp(segment, "local") == 0 ||
-            strcmp(segment, "argument") == 0 ||
-            strcmp(segment, "this") == 0 ||
-            strcmp(segment, "that") == 0
-        ) {
-            fprintf(output_file, "@SP\n");
-            fprintf(output_file, "A=M\n");
-            fprintf(output_file, "M=D\n");
-            fprintf(output_file, "@SP\n");
-            fprintf(output_file, "M=M+1\n");
-        }
+        // Push value in D to stack and increase stack pointer
+        fprintf(output_file, "@SP\n");
+        fprintf(output_file, "A=M\n");
+        fprintf(output_file, "M=D\n");
+        fprintf(output_file, "@SP\n");
+        fprintf(output_file, "M=M+1\n");
     } else if (type == C_POP) {
+        if (strcmp(segment, "constant") == 0) {
+            fprintf(stderr, "Error: cannot pop to constant segment\n");
+            return;
+        }
 
-    } else if (type == C_ARITHMETIC) {
+        // Save the segment address into R13
+        if (strcmp(segment, "temp") == 0) {
+            if (index < 0 || index > 7) {
+                fprintf(stderr, "Error: temp index can only go from range 0 to 7");
+                return;
+            } else {
+                fprintf(output_file, "@%d\n", index + 5);
+                fprintf(output_file, "D=A\n");
+                fprintf(output_file, "@R13\n");
+                fprintf(output_file, "M=D\n");
+            }
+        } else if (strcmp(segment, "pointer") == 0) {
+            if (index == 0 || index == 1) {
+                if (index == 0) {
+                    fprintf(output_file, "@THIS\n");
+                } else {
+                    fprintf(output_file, "@THAT\n");
+                }
+                fprintf(output_file, "D=A\n");
+                fprintf(output_file, "@R13\n");
+                fprintf(output_file, "M=D\n");
+            } else {
+                fprintf(stderr, "Error: pointer index can only be 0 or 1");
+                return;
+            }
+        } else if (base != NULL) {
+            fprintf(output_file, "@%d\n", index);
+            fprintf(output_file, "D=A\n");
+            fprintf(output_file, "@%s\n", base);
+            fprintf(output_file, "A=D+M\n");
+            fprintf(output_file, "D=A\n");
+            fprintf(output_file, "@R13\n");
+            fprintf(output_file, "M=D\n");
+        } else {
+            fprintf(stderr, "Error: Unknown segment in pop");
+            return;
+        }
+        
 
+        // Assign value from stack to segment memory that save in R13
+        fprintf(output_file, "@SP\n");
+        fprintf(output_file, "M=M-1\n");
+        fprintf(output_file, "A=M\n");
+        fprintf(output_file, "D=M\n");
+        fprintf(output_file, "@R13\n");
+        fprintf(output_file, "A=M\n");
+        fprintf(output_file, "M=D\n");
     }
 }
 
